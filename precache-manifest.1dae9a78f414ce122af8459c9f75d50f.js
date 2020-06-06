f693e446b75a01265896d549617759ac6a79f5c7
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-06-06 22:57:43 +0530

    Updates

---
+++ precache-manifest.1dae9a78f414ce122af8459c9f75d50f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad620d27f71a03083c93b05d3e27c1d1",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "b619cb6f9380d4caeb7e",
+    "url": "/react-currency-converter/static/css/main.0998f0bd.chunk.css"
+  },
+  {
+    "revision": "89379e22609b4ce70665",
+    "url": "/react-currency-converter/static/js/2.a5da546d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-converter/static/js/2.a5da546d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b619cb6f9380d4caeb7e",
+    "url": "/react-currency-converter/static/js/main.ab4d854a.chunk.js"
+  },
+  {
+    "revision": "5e7e6947b534306b9f27",
+    "url": "/react-currency-converter/static/js/runtime-main.53c81b47.js"
+  }
+]);
